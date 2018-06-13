32b328b2a2c3f15fb880478728950a5137483f45
Ilan <ilantb@gmail.com>
2018-06-13 15:20:36 +0300

    fixed the bug with the cookie

--- Week10L3ex1.py
+++ Week10L3ex1.py
@@ -2,7 +2,7 @@ import bottle as b
 from bottle import route, run, get, request, response
 import json
 import feedparser
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 feed = feedparser.parse("https://www.jpost.com/Rss/RssFeedsHeadlines.aspx")
@@ -14,10 +14,18 @@ def index():
 
 @get('/api/getNews')
 def get_news():
+    now_time = str(datetime.now())
+    if request.get_cookie("last_log") :
+        msg = "Welcome back, your last visit was : ", request.get_cookie("last_log")
+        response.set_cookie("last_log", now_time)
+    else :
+        expire_time = datetime.now()+timedelta(days=13)
+        response.set_cookie("last_log", now_time, expires = expire_time)
+        msg = "Welcome to rss"
     listNews = list()
     for element in feed["entries"]:
         listNews.append({"title": element["title"], "link": element["link"]})
-    return json.dumps(listNews)
+    return json.dumps([listNews,msg])
 
 
 def main():
